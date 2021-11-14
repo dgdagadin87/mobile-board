a90335b76c533e680bff080ce3fb960502c25afc
Дмитрий Дагадин <dagadin@jamm.ru>
2021-11-14 19:14:07 +0300

    New video screen was modified

--- screens/NewVideoScreen.tsx
+++ screens/NewVideoScreen.tsx
@@ -1,17 +1,63 @@
 import * as React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import { Text, View } from '../components/Themed';
+import { ImageBackground, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, Text } from '../components/Themed';
 import WithAuth from '../components/hocs/WithAuth';
+import { AppLogo } from '../components/AppLogo';
+import AlertService from '../services/alert-service';
+
 
 class NewVideoScreen extends React.Component<any> {
+
+	private alert = AlertService;
+
+	constructor(props: any) {
+		super(props);
+
+		this.startVideoClick = this.startVideoClick.bind(this);
+	}
+
+	startVideoClick() {
+		this.alert.alert('', 'Запись видео началась!');
+	}
+
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text style={styles.title}>It is new video</Text>
-				<TouchableOpacity style={styles.link}>
-					<Text style={styles.linkText}>Go to home screen!</Text>
-				</TouchableOpacity>
-			</View>
+			<ImageBackground
+				source={require('../assets/images/auth_bg.png')}
+				resizeMode="cover"
+				style={styles.image}
+			>
+				<View style={styles.container}>
+
+					<AppLogo />
+
+					<TouchableOpacity
+						activeOpacity={0.5}
+						onPress={this.startVideoClick}
+						style={styles.startVideoButton}
+					>
+						<Image
+							style={{ width: 100, height: 87.4 }}
+							source={require('../assets/images/startvideo.png')}
+						/>
+					</TouchableOpacity>
+
+					<View style={styles.banner}>
+						<Text style={styles.bannerTitle}>
+							чтобы записать видео нужно перевернуть устройство горизонтально
+						</Text>
+						<View style={styles.demoControl}>
+							<View style={styles.demoControlInner} />
+						</View>
+						<View style={styles.bannerDescription}>
+							<Text style={styles.bannerDescriptionText}>
+								Нажмите кнопку сверху,{"\n"}когда будете готовы
+							</Text>
+						</View>
+					</View>
+
+				</View>
+			</ImageBackground>
 		);
 	}
 }
@@ -20,21 +66,71 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: 'center',
-		justifyContent: 'center',
-		padding: 20,
+		backgroundColor: 'transparent'
 	},
-	title: {
-		fontSize: 20,
+	image: {
+		flex: 1,
+		justifyContent: "center",
+		width: '100%',
+		height: '100%'
+	},
+	startVideoButton: {
+		width: 100,
+		height: 87.4,
+		marginTop: 200
+	},
+	banner: {
+		width: '90%',
+		backgroundColor: '#969692',
+		borderRadius: 10,
+		marginTop: 30,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 5,
+	},
+	bannerTitle: {
+		marginLeft: 10,
+		marginRight: 10,
+		fontSize: 18,
 		fontWeight: 'bold',
+		marginTop: 10,
+		textAlign: 'center',
+		color: 'white',
+		textTransform: 'uppercase',
 	},
-	link: {
+	demoControl: {
+		backgroundColor: 'transparent',
+		borderTopColor: '#ffffff',
+		borderTopWidth: 2,
+		borderBottomColor: '#ffffff',
+		borderBottomWidth: 2,
 		marginTop: 15,
-		paddingVertical: 15,
+		padding: 10,
+		width: '30%',
+		marginLeft: '35%'
 	},
-	linkText: {
-		fontSize: 14,
-		color: '#2e78b7',
+	demoControlInner: {
+		margin: 'auto',
+		width: '90%',
+		height: 20,
+		backgroundColor: '#ffffff'
 	},
+	bannerDescription: {
+		marginTop: 20,
+		backgroundColor: 'transparent',
+		marginBottom: 10
+
+	},
+	bannerDescriptionText: {
+		fontSize: 16,
+		color: '#ffffff',
+		textAlign: 'center',
+	}
 });
 
 const extendedComponent = WithAuth(NewVideoScreen);
