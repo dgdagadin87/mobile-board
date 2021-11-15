e231e2cd9b2c305cc4df616fbec929f7773f094a
Дмитрий Дагадин <dagadin@jamm.ru>
2021-11-15 12:59:10 +0300

    Fix

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -9,21 +9,23 @@ export default function useCachedResources() {
 	// Load any resources or data that we need prior to rendering the app
 	React.useEffect(() => {
 		async function loadResourcesAndDataAsync() {
-		try {
-			SplashScreen.preventAutoHideAsync();
+			try {
+				await SplashScreen.preventAutoHideAsync();
 
-			// Load fonts
-			await Font.loadAsync({
-			...FontAwesome.font,
-			'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
-			});
-		} catch (e) {
-			// We might want to provide this error information to an error reporting service
-			console.warn(e);
-		} finally {
-			setLoadingComplete(true);
-			SplashScreen.hideAsync();
-		}
+				// Load fonts
+				await Font.loadAsync({
+					...FontAwesome.font,
+					'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+				});
+			}
+			catch (e) {
+				// We might want to provide this error information to an error reporting service
+				console.warn(e);
+			}
+			finally {
+				setLoadingComplete(true);
+				await SplashScreen.hideAsync();
+			}
 		}
 
 		loadResourcesAndDataAsync();
