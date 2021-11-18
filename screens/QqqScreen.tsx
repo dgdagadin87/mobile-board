9e508bd336be1ef04bacc75a478dfc278e77543a
Дмитрий Дагадин <dagadin@jamm.ru>
2021-11-18 11:49:29 +0300

    Commit

--- screens/QqqScreen.tsx
+++ screens/QqqScreen.tsx
@@ -1,75 +1,149 @@
-'use strict';
-import React, { PureComponent } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import React, { useState, useEffect, useRef, PureComponent } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { Camera } from 'expo-camera';
+import { Video } from 'expo-av';
 
-export default class QqqScreen extends PureComponent {
-
-	public camera: any
-
+class Preview extends PureComponent<any> {
 	render() {
+	  const { 
+		video, 
+	  } = this.props;
+	  if (video && video.uri) {
+		const { height, width } = Dimensions.get('window');
 		return (
-			<View style={styles.container}>
-				<RNCamera
-					ref={ref => {
-						this.camera = ref;
-					}}
-					style={styles.preview}
-					type={RNCamera.Constants.Type.back}
-					flashMode={RNCamera.Constants.FlashMode.on}
-					androidCameraPermissionOptions={{
-						title: 'Permission to use camera',
-						message: 'We need your permission to use your camera',
-						buttonPositive: 'Ok',
-						buttonNegative: 'Cancel',
-					}}
-					captureAudio={false}
-					androidRecordAudioPermissionOptions={{
-						title: 'Permission to use audio recording',
-						message: 'We need your permission to use your audio',
-						buttonPositive: 'Ok',
-						buttonNegative: 'Cancel',
-					}}
-					onGoogleVisionBarcodesDetected={({ barcodes }) => {
-						console.log(barcodes);
-					}}
-				/>
-				<View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-					<TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-						<Text style={{ fontSize: 14 }}> SNAP </Text>
-					</TouchableOpacity>
-				</View>
-			</View>
+		  <View style={styles.container}>
+			<Video 
+			  source={{ uri: video.uri }}
+			  style={[styles.image, { height, width }]}
+			  rate={1.0}
+			  isMuted={false}
+			  resizeMode="cover"
+			  volume={0.5}
+			  isLooping
+			  shouldPlay
+			/>
+		  </View>
 		);
+	  }
+	  return null;
 	}
+  }
 
-	takePicture = async () => {
-		if (this.camera) {
-			const options = { quality: 0.5, base64: true };
-			const data = await this.camera.takePictureAsync(options);
-			console.log(data.uri);
-		}
-	};
-}
 
-const styles = StyleSheet.create({
+  const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		flexDirection: 'column',
-		backgroundColor: 'black',
+	  position: 'absolute',
+	  top: 0, 
+	  left: 0,
+	  width: '100%',
+	  height: '100%'
 	},
-	preview: {
-		flex: 1,
-		justifyContent: 'flex-end',
-		alignItems: 'center',
-	},
-	capture: {
-		flex: 0,
-		backgroundColor: '#fff',
-		borderRadius: 5,
-		padding: 15,
-		paddingHorizontal: 20,
-		alignSelf: 'center',
-		margin: 20,
-	},
-});
+	image: {
+	  flex: 1,
+	  transform: [{
+		scaleX: -1
+	  }]
+	}
+  });
+
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState<any>(null);
+  const [cameraRef, setCameraRef] = useState<any>(null)
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [video, setVideo] = useState<any>(null);
+  const [recording, setRecording] = useState<boolean>(false)
+useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={type} ref={ref => {
+        setCameraRef(ref);
+  }}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            justifyContent: 'flex-end'
+          }}>
+            <View style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-evenly'
+            }}>
+
+          <TouchableOpacity style={{alignSelf: 'center'}} onPress={async() => {
+            if(cameraRef){
+              let photo = await cameraRef.takePictureAsync();
+              console.log('photo', photo);
+            }
+          }}>
+            <View style={{ 
+               borderWidth: 2,
+               borderRadius:25,
+               borderColor: 'white',
+               height: 50,
+               width:50,
+               display: 'flex',
+               justifyContent: 'center',
+               alignItems: 'center'}}
+            >
+              <View style={{
+                 borderWidth: 2,
+                 borderRadius:25,
+                 borderColor: 'white',
+                 height: 40,
+                 width:40,
+                 backgroundColor: 'white'}} >
+              </View>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{alignSelf: 'center'}} onPress={async() => {
+              if(!recording){
+                setRecording(true)
+              let video = await cameraRef.recordAsync();
+              setVideo(video)
+            } else {
+                setRecording(false)
+                cameraRef.stopRecording()
+            }
+          }}>
+            <View style={{ 
+               borderWidth: 2,
+               borderRadius:25,
+               borderColor: 'red',
+               height: 50,
+               width:50,
+               display: 'flex',
+               justifyContent: 'center',
+               alignItems: 'center'}}
+            >
+              <View style={{
+                 borderWidth: 2,
+                 borderRadius:25,
+                 borderColor: 'red',
+                 height: 40,
+                 width:40,
+                 backgroundColor: 'red'}} >
+              </View>
+            </View>
+          </TouchableOpacity>
+            </View>
+        </View>
+      </Camera>
+
+				<Preview video={video} />
+
+    </View>
+  );
+}
